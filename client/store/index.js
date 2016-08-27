0117a2cdfa1496d8a9ab69a379c49fcb8bf7b147
xwartz <stddup@gmail.com>
2016-08-27 13:41:53 +0800

    fix build production

--- client/store/index.js
+++ client/store/index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./configureStore.prod')
+  module.exports = require('./configureStore.pro')
 } else {
   module.exports = require('./configureStore.dev')
 }
